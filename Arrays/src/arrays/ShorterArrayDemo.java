31f5cf3f4e9f2e5204ef695ae73ce5959ec8bf7e
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-08 11:22:11 -0700

    Uses another way to declare arrays to be shorter.

---
+++ Arrays/src/arrays/ShorterArrayDemo.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class ShorterArrayDemo {
+	public static void main(String[] args) {
+		int[] anArray = {100, 200, 300, 400, 500,
+						600, 700, 800, 900, 1000};
+		for (int i : anArray) {
+			System.out.println(i);
+		}
+	}
+}
