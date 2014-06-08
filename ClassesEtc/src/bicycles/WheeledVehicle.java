530621d181761814030dab0ea2a5ef901450fd92
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-08 10:05:48 -0700

    Interface for a wheeled vehicle

---
+++ ClassesEtc/src/bicycles/WheeledVehicle.java
@@ -0,0 +1,8 @@
+package bicycles;
+
+public interface WheeledVehicle {
+	public void setWheels(int wheels);
+	public void setGear(int newGear);
+	public void setSeats(int seats);
+	public void brakes(boolean braking);
+}
