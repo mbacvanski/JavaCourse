7ffebd1f6ad2aa44928c03600ec5dfa5477d9005
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-07 22:47:18 -0700

    Second commit, and made .gitignore file.

---
+++ Hello World App/src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,21 @@
+/**
+ * Prints "Hello World" to standard output.
+ */
+
+package helloworldapp;
+
+/**
+ * @author Marc
+ *
+ */
+public class HelloWorldApp {
+
+	/**
+	 * @param Takes no arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
