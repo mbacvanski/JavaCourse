6ac2e44bc3aec5e317f6a2ed3e46f4395b079b5e
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-08 21:41:23 -0700

    Better print method, and used another way to declare an array.

--- Arrays/src/arrays/MoreMultiDimArrays.java
+++ Arrays/src/arrays/MoreMultiDimArrays.java
@@ -9,6 +9,9 @@ public class MoreMultiDimArrays {
 		int[][] multiplicationTable;
 		multiplicationTable = new int[rowNumber][columnNumber];
 		
+		int[][] newMultiplicationTable = {{1, 2, 3, 4, 5}, {2, 4, 6, 8, 10}, {3, 6, 9, 12, 15},
+											{4, 8, 12, 16, 20}, {5, 10, 15, 20, 25}};
+		
 		int totalRows = multiplicationTable.length;
 		
 		for (int row = 0; row < totalRows; row++) {
@@ -20,7 +23,23 @@ public class MoreMultiDimArrays {
 
 		}
 		
-		System.out.println(multiplicationTable);
+		for (int row = 0; row < totalRows; row++) {
+			int ColumnsInCurrentRow = multiplicationTable[row].length;
+			System.out.println();
+			for (int column = 0; column < ColumnsInCurrentRow; column++) {
+				System.out.print(multiplicationTable[row][column] + "    ");
+			}
+
+		}
+		
+		for (int row = 0; row < 5; row++) {
+			int ColumnsInCurrentRow = newMultiplicationTable[row].length;
+			System.out.println();
+			for (int column = 0; column < ColumnsInCurrentRow; column++) {
+				System.out.print(newMultiplicationTable[row][column] + "    ");
+			}
+
+		}
 	}
 
 }
