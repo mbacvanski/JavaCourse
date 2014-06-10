549860d74590c7a07de0fc47443dd293572c68f2
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-09 17:31:29 -0700

    Program that demonstrates the arithmetic operations.

---
+++ Operations/src/variables/Arithmetic.java
@@ -0,0 +1,29 @@
+package variables;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int sum;
+		int difference;
+		int product;
+		double quotient;
+		int modulo;
+		String jeeb;
+		//Declare variables
+		sum = 70 + 42;
+		difference = 70 - 42;
+		product = 70 * 42;
+		quotient = 70.0 / 42.0;
+		modulo = 70 % 42;
+		jeeb = "Hello " + "World!";
+		//Do operations on the variables
+		System.out.println("70 + 42 = " + sum);
+		System.out.println("70 - 42 = " + difference);
+		System.out.println("70 * 42 = " + product);
+		System.out.println("70 / 42 = " + quotient);
+		System.out.println("70 % 42 = " + modulo);
+		System.out.println("Hello + World = " + jeeb);
+		//Print everything out!
+	}
+
+}
