3e0e700fd36ba6c74ef1a46f2d5781ebe3d69cae
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-12 16:52:03 -0700

    Added comments

--- Operations/src/operations/Unary.java
+++ Operations/src/operations/Unary.java
@@ -10,22 +10,24 @@ public class Unary {
 		int unaryDecrement;
 		boolean unaryComplement;
 		boolean unaryOriginal;
-		
+		//Declare variables!
 		unaryNegative = -42;
 		unaryPositive = +42;
-		
+		//Demonstrate positive and negative
 		originalNum = 42;
 		unaryIncrement = ++originalNum;
+		//Demonstrate increments
 		originalNum = 42;
 		unaryDecrement = --originalNum;
-		
+		//Demonstrate decrements
 		unaryOriginal = true;
 		unaryComplement = !unaryOriginal;
-		
+		//Demonstrate negation
 		System.out.println("The positive of " + unaryNegative + " is " + unaryPositive);
 		System.out.println("The increment of " + originalNum + " is " + unaryIncrement);
 		System.out.println("The decrement of " + originalNum + " is " + unaryDecrement);
 		System.out.println("The complement of " + unaryOriginal + " is "+ unaryComplement);
+		//Print everything out!
 	}
 
 }
