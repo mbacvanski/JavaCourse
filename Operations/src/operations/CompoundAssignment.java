d367bcab42a731905d96dece1a17e2b738be2933
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-12 20:39:42 -0700

    Demonstrates using compound assignment statements.

---
+++ Operations/src/operations/CompoundAssignment.java
@@ -0,0 +1,23 @@
+package operations;
+
+class CompoundAssignment {
+
+    public static void main (String[] args){
+         
+         int result = 1 + 2; // result is now 3
+         System.out.println(result);
+
+         result = --result; // result is now 2
+         System.out.println(result);
+
+         result *= 2; // result is now 4
+         System.out.println(result);
+
+         result /= 2; // result is now 2
+         System.out.println(result);
+
+         result += 8; // result is now 10
+         result %= 7; // result is now 3
+         System.out.println(result);
+    }
+}
