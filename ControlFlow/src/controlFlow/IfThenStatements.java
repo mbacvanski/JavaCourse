4fd55dd3a8ded06de2d4bfb9ed03591bb58d3d11
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-12 20:56:07 -0700

    Demonstrates the use of if - then statements.

---
+++ ControlFlow/src/controlFlow/IfThenStatements.java
@@ -0,0 +1,32 @@
+package controlFlow;
+
+public class IfThenStatements {
+
+	public static void main(String[] args) {
+		boolean bool1 = true;
+		boolean bool2 = false;
+		boolean bool3 = true;
+		if (bool1) {
+			System.out.println("The first boolean is true!");
+			if (bool2) {
+				System.out.println("The second boolean is also true!");
+				if (bool3) {
+					System.out.println("The third boolean is also true!");
+				}
+			}
+		}
+		if (bool2) {
+			System.out.println("The second boolean is true!");
+			if (bool3) {
+				System.out.println("The third boolean is also true!");
+			}
+		}
+		if (bool3) {
+			System.out.println("The third boolean is true!");
+		}
+		else {
+			System.out.println("Something was wrong...");
+		}
+	}
+
+}
