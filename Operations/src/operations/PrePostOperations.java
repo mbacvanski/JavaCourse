d390fc6106e6ee485a794680e0014e0358f49674
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-12 17:00:20 -0700

    Class to demonstrate the proper order of prefix/postfix unary increments/decrements.

---
+++ Operations/src/operations/PrePostOperations.java
@@ -0,0 +1,18 @@
+package operations;
+
+public class PrePostOperations {
+
+	public static void main(String[] args) {
+		int num = 42;
+		//Declares & assigns num to 42.
+		System.out.println("Num is equal to: " + num);
+		System.out.println("Num++ is equal to: " + num++);
+		num = 42;
+		System.out.println("++Num is equal to: " + ++num);
+		num = 42;
+		System.out.println("Num-- is equal to: " + num--);
+		num = 42;
+		System.out.println("--Num is equal to: " + --num);
+	}
+
+}
