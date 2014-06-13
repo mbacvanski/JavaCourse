8adfddfbd3f5263d69c70d76b01ad71848b96610
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-12 21:32:13 -0700

    Used constants instead of literals.

--- ControlFlow/src/controlFlow/Switches.java
+++ ControlFlow/src/controlFlow/Switches.java
@@ -2,23 +2,37 @@ package controlFlow;
 
 public class Switches {
 
+	private static final String INVALID_MONTH = "Invalid month";
+	private static final String DECEMBER = "December";
+	private static final String NOVEMBER = "November";
+	private static final String OCTOBER = "October";
+	private static final String SEPTEMBER = "September";
+	private static final String JULY = "July";
+	private static final String JUNE = "June";
+	private static final String MAY = "May";
+	private static final String APRIL = "April";
+	private static final String MARCH = "March";
+	private static final String FEBRUARY = "February";
+	private static final String JANUARY = "January";
+	private static final String AUGUST = "August";
+
 	public static void main(String[] args) {
 		int month = 8;
 		String monthString;
 		switch(month) {
-			case 1: {monthString = "January"; break;}
-			case 2: {monthString = "February"; break;}
-			case 3: {monthString = "March"; break;}
-			case 4: {monthString = "April"; break;}
-			case 5: {monthString = "May"; break;}
-			case 6: {monthString = "June"; break;}
-			case 7: {monthString = "July"; break;}
-			case 8: {monthString = "August"; break;}
-			case 9: {monthString = "September"; break;}
-			case 10: {monthString = "October"; break;}
-			case 11: {monthString = "November"; break;}
-			case 12: {monthString = "December"; break;}
-			default: {monthString = "Invalid month"; break;}
+			case 1: {monthString = JANUARY; break;}
+			case 2: {monthString = FEBRUARY; break;}
+			case 3: {monthString = MARCH; break;}
+			case 4: {monthString = APRIL; break;}
+			case 5: {monthString = MAY; break;}
+			case 6: {monthString = JUNE; break;}
+			case 7: {monthString = JULY; break;}
+			case 8: {monthString = AUGUST; break;}
+			case 9: {monthString = SEPTEMBER; break;}
+			case 10: {monthString = OCTOBER; break;}
+			case 11: {monthString = NOVEMBER; break;}
+			case 12: {monthString = DECEMBER; break;}
+			default: {monthString = INVALID_MONTH; break;}
 		}
 		System.out.println(monthString);
 	}
