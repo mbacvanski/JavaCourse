6ae09f328413f8684adc79e8743408906bdd622d
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-25 11:40:49 -0700

    Demonstrates a simple while loop.

---
+++ ControlFlow/src/controlFlow/WhileDemo.java
@@ -0,0 +1,13 @@
+package controlFlow;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int count = 5;
+		while (count > 0) {
+			System.out.println(count);
+			count--;
+		}
+	}
+
+}
