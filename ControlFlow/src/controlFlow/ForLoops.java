4a60d1b37dde2a2cea36b5b4cb0419a99c10f9bd
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-29 21:28:46 -0700

    Demonstrates the use of for loops

---
+++ ControlFlow/src/controlFlow/ForLoops.java
@@ -0,0 +1,11 @@
+package controlFlow;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 42; i++) {
+			System.out.println("The count right now is: " + i);
+		}
+	}
+
+}
