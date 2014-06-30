7a519c3ad0daf326a55349f6c4fd7697d9dd8170
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-06-29 21:31:02 -0700

    Demonstrates an enhanced for loop.

--- ControlFlow/src/controlFlow/ForLoops.java
+++ ControlFlow/src/controlFlow/ForLoops.java
@@ -6,6 +6,12 @@ public class ForLoops {
 		for (int i = 1; i <= 42; i++) {
 			System.out.println("The count right now is: " + i);
 		}
+		
+		int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		
+		for (int j : numbers	) {
+			System.out.println("The other count right now is: " + j);
+		}
 	}
 
 }
