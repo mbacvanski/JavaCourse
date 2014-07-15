1d82edb86229747e893818cdc29269029118f8ec
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-07-14 22:40:54 -0700

    Demos the roundThingsCalculator

---
+++ EvenMoreClasses/src/evenMoreClasses/RoundThingsCalculatorDemo.java
@@ -0,0 +1,10 @@
+package evenMoreClasses;
+
+public class RoundThingsCalculatorDemo {
+
+	public static void main(String[] args) {
+		RoundThingsCalculator thingy = new RoundThingsCalculator();
+		thingy.main(args);
+	}
+
+}
