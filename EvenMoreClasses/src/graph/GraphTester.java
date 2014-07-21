29f7107debed4a43ccbac4222edeff00696270a8
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-07-21 14:26:08 -0700

    Updated the tester to test more things.

--- EvenMoreClasses/src/graph/GraphTester.java
+++ EvenMoreClasses/src/graph/GraphTester.java
@@ -1,10 +1,13 @@
 package graph;
 
+import java.util.ArrayList;
+
 public class GraphTester {
 
 	private static final String CHUKAR = "Chukar";
 	private static final String ANDROID = "Android";
 	private static final String PIGGY = "Piggy";
+	private static final String POW = "Pow";
 
 	public static void main(String[] args) {
 		Graph graph = new Graph();
@@ -15,7 +18,16 @@ public class GraphTester {
 		graph.makeConnection(CHUKAR, ANDROID);
 		System.out.println("Android's connections: " + graph.getConnections(ANDROID));
 		System.out.println("Chukar's connections: " + graph.getConnections(CHUKAR));
-		System.out.println("Piggy's connectionss: " + graph.getConnections(PIGGY));
+		System.out.println("Piggy's connections: " + graph.getConnections(PIGGY));
+		ArrayList<String> powsConnections = new ArrayList<>();
+		powsConnections.add(ANDROID);
+		powsConnections.add(CHUKAR);
+		powsConnections.add(PIGGY);
+		graph.addNode(POW, powsConnections);
+		System.out.println("Pow's connections: " + graph.getConnections(POW));
+		System.out.println("Android's connections: " + graph.getConnections(ANDROID));
+		System.out.println("Chukar's connections: " + graph.getConnections(CHUKAR));
+		System.out.println("Piggy's connections: " + graph.getConnections(PIGGY));
 	}
 
 }
