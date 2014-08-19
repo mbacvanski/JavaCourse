bddb5479bd305f1c38d25ea713b20b89a5a063f3
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-08-18 21:43:23 -0700

    Updated the tester for flying cars.

--- Inheritance/src/flyingCars/FlyingCarTester.java
+++ Inheritance/src/flyingCars/FlyingCarTester.java
@@ -3,7 +3,7 @@ package flyingCars;
 public class FlyingCarTester {
 
 	public static void main(String[] args) {
-		FlyingCar flyCar = new FlyingCar();
+		FlyingCar2 flyCar = new FlyingCar2();
 		flyCar.startEngine();
 	}
 
