12dad04b3d190e46ec5d1d950fea2c44142071bc
Marc Bacvanski <marc.bacvanski@gmail.com>
2014-08-20 21:15:35 -0700

    Commented to make flying cars clearer.

--- Inheritance/src/flyingCars/FlyingCar2.java
+++ Inheritance/src/flyingCars/FlyingCar2.java
@@ -2,6 +2,7 @@ package flyingCars;
 
 public class FlyingCar2 implements FlyCar, OperateCar {
 	public void startEngine() {
+		//Keyword super
 		FlyCar.super.startEngine();
 		OperateCar.super.startEngine();
 	}
